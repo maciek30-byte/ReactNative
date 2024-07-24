6b13c6d25e41e37b00356a73429e2fb20dadb97f
maciek30-byte <maciek29.opozda@gmail.com>
2024-07-24 22:35:26 +0200

    BP-1 Initialise deps, configure jest

---
+++ __tests__/test.spec.ts
@@ -0,0 +1,5 @@
+describe('Test one', () => {
+    it('Test 1', () => {
+        expect(true).toBe(true)
+    })
+});
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js|jsx)?$',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+};
